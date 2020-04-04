4fb40c2b3b5b153d81156a927e510a41656650b9
Fabb111 <fabb111@nextglobe.net>
2020-04-04 19:18:05 +0200

    Added basic CloudManager functionality

---
+++ CloudManager/Base/src/main/kotlin/net/nextglobe/expresscloud/cm/CloudManager.kt
@@ -0,0 +1,113 @@
+package net.nextglobe.expresscloud.cm
+
+import net.nextglobe.expresscloud.api.server.Server
+import java.util.UUID
+
+interface CloudManager {
+
+    val implementationName: String
+
+    /**
+     * Optionally connect to the management solution (for example Docker). This function is called
+     * if the particular CloudManager implementation has been selected by the user (in the configuration)
+     */
+    fun connect(connectionString: String)
+
+    /**
+     * Optionally register listeneres here. This function is called
+     * if the particular CloudManager implementation has been selected by the user (in the configuration)
+     */
+    fun registerListeners()
+
+    /**
+     * Optionally disconnect from the management solution (for example Docker). This function is called
+     * if the particular CloudManager implementation has been selected by the user (in the configuration)
+     */
+    fun disconnect()
+
+    /**
+     * Used to prepare an image. For example, the docker implementation would
+     * create a new image (if it does not exist). A wrapper implementation would copy the needed files
+     * into a directory on all available machines
+     * @return true if the preparation was successful
+     */
+    suspend fun prepareImage(server: Server) : Boolean
+
+    /**
+     * See [prepareImage]
+     */
+    suspend fun prepareImages(servers: List<Server>) : Boolean
+
+
+    /**
+     * Used to create a server. For example, the docker implementation would
+     * first call [prepareImage] and then a container would be created
+     * @return true if the creation was successful
+     */
+    suspend fun createServer(server: Server) : Boolean
+
+    /**
+     * See [createServer]
+     */
+    suspend fun createServers(servers: List<Server>) : Boolean
+
+
+    /**
+     * Used to start a server. For example, the docker implementation would
+     * first call [createServer] and then the created container would started
+     * @return true if the start was successful
+     */
+    suspend fun startServer(server: Server) : Boolean
+
+    /**
+     * See [startServer]
+     */
+    suspend fun startServers(servers: List<Server>) : Boolean
+
+
+    /**
+     * Used to stop a started server. For example, the docker implementation would
+     * first stop the created container and then [deleteServer] would be called
+     * @return true if the stop was successful
+     */
+    suspend fun stopServer(server: Server) : Boolean
+
+    /**
+     * See [stopServer]
+     */
+    suspend fun stopServers(servers: List<Server>) : Boolean
+
+    /**
+     * See [stopServer]
+     */
+    suspend fun stopServerById(serverId: UUID) : Boolean
+
+    /**
+     * See [stopServer]
+     */
+    suspend fun stopServersById(serverIds: List<UUID>) : Boolean
+
+
+    /**
+     * Used to delete a created server. For example the docker implementation would
+     * delete the created container
+     * @return true if the deletion was successful
+     */
+    suspend fun deleteServer(server: Server) : Boolean
+
+    /**
+     * See [deleteServer]
+     */
+    suspend fun deleteServers(servers: List<Server>) : Boolean
+
+    /**
+     * See [deleteServer]
+     */
+    suspend fun deleteServerById(serverId: UUID) : Boolean
+
+    /**
+     * See [deleteServer]
+     */
+    suspend fun deleteServersById(serverIds: List<UUID>) : Boolean
+
+}
---
+++ CloudManager/Implementations/Docker/src/main/kotlin/net/nextglobe/expresscloud/implementation/docker/DockerCloudManager.kt
@@ -0,0 +1,129 @@
+package net.nextglobe.expresscloud.implementation.docker
+
+import com.github.dockerjava.api.DockerClient
+import com.github.dockerjava.api.command.DockerCmdExecFactory
+import com.github.dockerjava.core.DefaultDockerClientConfig
+import com.github.dockerjava.core.DockerClientConfig
+import com.github.dockerjava.core.DockerClientImpl
+import com.github.dockerjava.netty.NettyDockerCmdExecFactory
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+import mu.KotlinLogging
+import net.nextglobe.expresscloud.api.server.Server
+import net.nextglobe.expresscloud.cm.CloudManager
+import java.util.*
+
+val logger = KotlinLogging.logger {}
+
+class DockerCloudManager : CloudManager {
+
+    private val NETWORK_NAME: String = "expresscloud-docker-network"
+    private val IMAGE_PREFIX: String = "expresscloud-docker-"
+
+    private lateinit var dockerClient: DockerClient
+
+    override val implementationName: String = "Docker"
+
+    override fun connect(connectionString: String) {
+        logger.info { "Connecting to \"$connectionString\" without TLS..." }
+
+        val dockerClientConfig: DockerClientConfig = DefaultDockerClientConfig.createDefaultConfigBuilder()
+            .withDockerHost(connectionString)
+//            .withDockerTlsVerify(true)
+//            .withDockerCertPath("/home/user/.docker")
+            .build()
+
+        val dockerCmdExecFactory: DockerCmdExecFactory = NettyDockerCmdExecFactory().withConnectTimeout(10000)
+
+        dockerClient = DockerClientImpl.getInstance(dockerClientConfig).withDockerCmdExecFactory(dockerCmdExecFactory)
+
+        if(dockerClient.listNetworksCmd().withNameFilter(NETWORK_NAME).exec().count() == 0) {
+            logger.info { "Creating custom network ($NETWORK_NAME)..." }
+            val id = dockerClient.createNetworkCmd().withName(NETWORK_NAME).withOptions(mapOf(Pair("subnet", "1.0.0.0/24"))).exec().id
+            logger.debug { "Created network with id $id" }
+        }
+        logger.info { "Stopping & deleting all ExpressCloud containers..." }
+        runBlocking {
+            dockerClient.listContainersCmd().withNetworkFilter(listOf(NETWORK_NAME)).exec().forEach {
+                launch {
+                    dockerClient.stopContainerCmd(it.id).exec()
+                    dockerClient.removeContainerCmd(it.id).exec()
+                    logger.debug { "Stopped & deleted container ${it.names[0]} (${it.id})" }
+                }
+            }
+        }
+        logger.info { "Initialization finished!" }
+    }
+
+    override fun registerListeners() {
+//        dockerClient.eventsCmd().
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun disconnect() {
+        dockerClient.close()
+    }
+
+    override suspend fun prepareImage(server: Server): Boolean {
+//        if(server is CategorizedServer) {
+//            dockerClient.createImageCmd(IMAGE_PREFIX + server.category.name, )
+////            server.category.name
+//        } else {
+//            throw IllegalArgumentException("The provided server has no image information")
+//        }
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override suspend fun prepareImages(servers: List<Server>): Boolean {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override suspend fun createServer(server: Server): Boolean {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override suspend fun createServers(servers: List<Server>): Boolean {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override suspend fun startServer(server: Server): Boolean {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override suspend fun startServers(servers: List<Server>): Boolean {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override suspend fun stopServer(server: Server): Boolean {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override suspend fun stopServerById(serverId: UUID): Boolean {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override suspend fun stopServers(servers: List<Server>): Boolean {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override suspend fun stopServersById(serverIds: List<UUID>): Boolean {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override suspend fun deleteServer(server: Server): Boolean {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override suspend fun deleteServerById(serverId: UUID): Boolean {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override suspend fun deleteServers(servers: List<Server>): Boolean {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override suspend fun deleteServersById(serverIds: List<UUID>): Boolean {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+}
