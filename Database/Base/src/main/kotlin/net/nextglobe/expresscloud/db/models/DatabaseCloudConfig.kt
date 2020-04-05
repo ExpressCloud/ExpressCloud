34a9881f1b6deec5fab89b6dc3889aa3b57af721
Fabb111 <fabb111@nextglobe.net>
2020-04-05 15:17:48 +0200

    Added functions to find the active config in a list of configs

--- Database/Base/src/main/kotlin/net/nextglobe/expresscloud/db/models/DatabaseCloudConfig.kt
+++ Database/Base/src/main/kotlin/net/nextglobe/expresscloud/db/models/DatabaseCloudConfig.kt
@@ -1,6 +1,7 @@
 package net.nextglobe.expresscloud.db.models
 
 import kotlinx.serialization.Serializable
+import net.nextglobe.expresscloud.api.exception.CloudException
 import net.nextglobe.expresscloud.db.UUIDSerializer
 import java.util.UUID
 
@@ -11,4 +12,19 @@ data class DatabaseCloudConfig(
     val name: String,
     val active: Boolean,
     val cloudManagerImplementation: String
-)
+) {
+    companion object {
+        fun getActiveDatabaseCloudConfig(cloudConfigs: List<DatabaseCloudConfig>) : DatabaseCloudConfig? {
+            return cloudConfigs.find { it.active }
+        }
+
+        fun getActiveDatabaseCloudConfigStrict(cloudConfigs: List<DatabaseCloudConfig>) : DatabaseCloudConfig {
+            val activeDatabaseCloudConfigs = cloudConfigs.filter { it.active }
+            if(activeDatabaseCloudConfigs.size > 1)
+                throw CloudException("More than one database cloud configs are active. Only one config can be active at a time")
+            else if(activeDatabaseCloudConfigs.isEmpty())
+                throw CloudException("No database cloud config is active. Exactly one config must be active at a time")
+            return activeDatabaseCloudConfigs.first()
+        }
+    }
+}
