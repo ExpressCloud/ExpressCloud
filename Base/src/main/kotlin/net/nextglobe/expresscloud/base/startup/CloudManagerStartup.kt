77fc724492afa17da6961b668223dccb665cfb44
Fabb111 <fabb111@nextglobe.net>
2020-04-05 15:18:59 +0200

    Load CloudManager implementation from DatabaseCloudConfig

--- Base/src/main/kotlin/net/nextglobe/expresscloud/base/startup/CloudManagerStartup.kt
+++ Base/src/main/kotlin/net/nextglobe/expresscloud/base/startup/CloudManagerStartup.kt
@@ -3,14 +3,15 @@ package net.nextglobe.expresscloud.base.startup
 import mu.KotlinLogging
 import net.nextglobe.expresscloud.api.exception.cloudmanager.CloudManagerException
 import net.nextglobe.expresscloud.cm.CloudManager
+import net.nextglobe.expresscloud.db.models.DatabaseCloudConfig
 import net.nextglobe.expresscloud.implementation.docker.DockerCloudManager
 
 object CloudManagerStartup {
 
     private val logger = KotlinLogging.logger {}
 
-    fun initializeCloudManager(): CloudManager {
-        val implementation = "DOCKER" // TODO load from config
+    fun initializeCloudManager(cloudConfig: DatabaseCloudConfig): CloudManager {
+        val implementation = cloudConfig.cloudManagerImplementation.toUpperCase()
         logger.info { "Selected cloud manager implementation: $implementation. Initializing..." }
         val cloudManager: CloudManager
         if (implementation == "DOCKER") {
