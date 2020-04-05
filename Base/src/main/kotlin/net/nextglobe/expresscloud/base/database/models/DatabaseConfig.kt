0d9edc721fb3cd8069ecd40201d9d827224d0130
Fabb111 <fabb111@nextglobe.net>
2020-04-05 14:50:58 +0200

    Removed database related files

--- Base/src/main/kotlin/net/nextglobe/expresscloud/base/database/models/DatabaseConfig.kt
+++
@@ -1,9 +0,0 @@
-package net.nextglobe.expresscloud.base.database.models
-
-import java.util.UUID
-
-data class DatabaseConfig(
-    val uuid: UUID,
-    val name: String,
-    val cloudManagerImplementation: String
-)
